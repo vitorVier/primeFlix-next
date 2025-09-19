c05931210711e45afc8b1c3911f4deb7c21b2af4
vitorVier <vierrvitor@gmail.com>
2025-09-18 22:03:58 -0300

    change-general-display

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/header";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer autoClose={3000} />
         <Header />
         {children}
       </body>
